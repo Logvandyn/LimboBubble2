d4835bcd7cf32bf3376eabe1b254a5f0114956bf
Logvandyn <logvandyn@yahoo.com>
2023-12-05 19:45:44 -0800

    Bubble Duration

---
+++ Limbo Bubble/Assets/Scripts/Bubble.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Logan Van Dyne
+ * 12/5/23
+ * Bubble Shrink
+ */
+
+public class Bubble : MonoBehaviour
+{
+    public static Bubble instance;
+    //How long the bubble lasts 
+    public float Duration = 5f;
+
+    // Make it shrink down to it's final size before deletion
+    public Vector3 EndScale = Vector3.one * .5f;
+
+    //Starting size (object transform)
+    Vector3 Scale;
+
+    // Linear interpolation time (start at 0, end at 1 /)
+    float t = 0;
+
+    void OnEnable()
+    {
+        // initialize  in OnEnable (Replace with awake or start depending on what happens with spawning)
+        Scale = transform.localScale;
+        t = 0;
+    }
+
+    void Update()
+    {
+        // Divide deltaTime by the duration to stretch out the time it takes for t to go from 0 to 1.
+        t += Time.deltaTime / Duration;
+
+        // Interpolate from base scale to the end scale 
+        Vector3 newScale = Vector3.Lerp(Scale, EndScale, t);
+        transform.localScale = newScale;
+
+        if (t > 1) //if time is over, detroy bubble
+        {
+            Destroy(this.gameObject); //(this) destroys the script, not the game bubble itself
+        }
+    }
+}
---
+++ Limbo Bubble/Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Logan Van Dyne
+ * 11/30/23
+ * Player spawner to create the bubble
+ */
+public class Player : MonoBehaviour
+{
+    
+}
